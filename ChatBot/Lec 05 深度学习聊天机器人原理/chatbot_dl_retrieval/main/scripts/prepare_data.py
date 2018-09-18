75ac5bc6ace436e32ea1905c438b74e88c5208d9
Zoe <740465595@qq.com>
2018-09-18 13:29:03 +0800

    解析prepare_data

--- ChatBot/Lec 05 深度学习聊天机器人原理/chatbot_dl_retrieval/main/scripts/prepare_data.py
+++ ChatBot/Lec 05 深度学习聊天机器人原理/chatbot_dl_retrieval/main/scripts/prepare_data.py
@@ -38,11 +38,11 @@ def tokenizer_fn(iterator):
     '''
     将可迭代对象，逐个按空格分词（英文可以），返回的分词存在元组中。
     '''
-    return (x.split("") for x in iterator)
+    return (x.split(" ") for x in iterator)
 
 def create_csv_iter(filename):
     '''
-    返回生成的
+    将CSV文件内容变成生成器，可迭代对象。
     Returns an iterator over a CSV file. Skips the header.
     '''
     with open(filename) as csvfile:
@@ -54,9 +54,24 @@ def create_csv_iter(filename):
         for row in reader:
             yield row
 
+def create_vocab(input_iter, min_frequency):
+    '''
+    创建词汇表，最低词频由min_frequency确定
+    Creates and returns a VocabularyProcessor object with the vocabulary
+  for the input iterator.
+    '''
+    vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
+        FLAGS.max_sentence_len,
+        min_frequency=min_frequency,
+        tokenizer_fn=tokenizer_fn
+    )
+    vocab_processor.fit(input_iter)
+    return vocab_processor
 
 
 
 if __name__=='__main__':
-    print(FLAGS.min_word_frequency)
+    c = create_csv_iter(TEST_PATH)
+    for i in range(2):
+        print(c.__next__())
 
