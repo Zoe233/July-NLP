241463e9cea16d213c9921dd7766cf592753d36a
Zoe <740465595@qq.com>
2018-10-01 14:35:38 +0800

    将myimages读取tfrecord的脚本文件在data_factory中注册后，执行脚本文件中的train_image_classifier.py即可

---
+++ TF/DATAGURU/Lec 09/HandWriteImageClassification/slim/train.sh
@@ -0,0 +1,10 @@
+cd /Users/zoe/Documents/GitHub/July-NLP/TF/DATAGURU/Lec\ 09/HandWriteImageClassification/slim
+python train_image_classifier.py \
+--train_dir model \
+--dataset_name myimages \
+--dataset_split_name train \
+--dataset_dir images \
+--batch_size 10 \
+--max_number_of_steps 10000 \
+--model_name inception_v3 \
+pause
